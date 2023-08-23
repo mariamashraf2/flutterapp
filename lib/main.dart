0722a66e73749c1d0dc9a3248985802b8c1c7cda
mariamashraf2 <142065602+mariamashraf2@users.noreply.github.com>
2023-08-24 01:51:50 +0300

    project commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
       ),
-      home:const loginscreen(),
+      home: const loginscreen(),
     );
   }
 }
-
